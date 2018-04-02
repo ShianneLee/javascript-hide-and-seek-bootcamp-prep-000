2bb180be8be61cbe9aabbc80c82567783550cd42
Shianne Lee <shiannelee@live.ca>
2018-04-02 02:09:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function increaseRankBy(n){
   var rank = document.querySelectorAll('.ranked-list li');
   for (var i = 0; i < rank.length; i++) {
     //var num = rank[i].innerHTML;
-    rank[i].innerHTML = (rank[i].innerHTML + n).toString();
+    rank[i].innerHTML = (rank[i].innerHTML + n).parseInt;
   }
 }

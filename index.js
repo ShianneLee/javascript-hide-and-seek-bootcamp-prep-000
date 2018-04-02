0335c8b0743aee0c319917d49b9fc24f4dbd26ff
Shianne Lee <shiannelee@live.ca>
2018-04-02 01:57:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  var rank = document.querySelectorAll('.ranked-list');
+  for (var i = 0; i < rank.length; i++) {
+    var num = rank[i].innerHTML;
+    rank[i].innerHTML = (num + n).toString();
+  }
+}
